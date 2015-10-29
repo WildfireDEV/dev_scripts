ec0dd1b9f9ddc96d3e1cb36b2ddf64ec4d1c3116
WildfireDEV <wildfiredev@gmail.com>
2015-10-29 21:45:02 +1100

    Change to M8 patches

--- sync.sh
+++ sync.sh
@@ -3,19 +3,17 @@
 # My script that syncs unmerged changes for my cm-13.0 builds
 # To use, simply just run the script before compiling
 #
-
-cd ~/CM13/device/htc/m7-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_m7-common refs/changes/45/114345/1 && git cherry-pick FETCH_HEAD
-cd ~/CM13/device/htc/m7-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_m7-common refs/changes/27/113927/4 && git cherry-pick FETCH_HEAD
-cd ~/CM13/device/htc/m7-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_m7-common refs/changes/26/113926/4 && git cherry-pick FETCH_HEAD
-cd ~/CM13/device/htc/m7-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_m7-common refs/changes/25/113925/3 && git cherry-pick FETCH_HEAD
-cd ~/CM13/device/htc/msm8960-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8960-common refs/changes/35/114135/3 && git cherry-pick FETCH_HEAD
-cd ~/CM13/device/htc/msm8960-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8960-common refs/changes/30/113530/3 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/msm8974-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8974-common refs/changes/72/114572/2 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/msm8974-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8974-common refs/changes/77/112377/2 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/msm8974-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8974-common refs/changes/78/112378/2 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/msm8974-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8974-common refs/changes/68/112368/2 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/msm8974-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8974-common refs/changes/69/112369/2 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/msm8974-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8974-common refs/changes/70/112370/2 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/msm8974-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8974-common refs/changes/71/112371/2 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/msm8974-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8974-common refs/changes/74/112374/2 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/msm8974-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8974-common refs/changes/16/115616/1 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/msm8974-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_msm8974-common refs/changes/35/111835/15 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/m8-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_m8-common refs/changes/73/115273/4 && git cherry-pick FETCH_HEAD
+cd ~/CM13/device/htc/m8-common && git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_htc_m8-common refs/changes/43/112343/2 && git cherry-pick FETCH_HEAD
 cd ~/CM13/vendor/cm && git fetch http://review.cyanogenmod.org/CyanogenMod/android_vendor_cm refs/changes/36/111336/21 && git cherry-pick FETCH_HEAD
-cd ~/CM13/frameworks/av && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/41/114341/2 && git cherry-pick FETCH_HEAD
-cd ~/CM13/system/core && git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_core refs/changes/66/114266/2 && git cherry-pick FETCH_HEAD
-cd ~/CM13/frameworks/native && git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/41/114241/8 && git cherry-pick FETCH_HEAD
-cd ~/CM13/hardware/libhardware_legacy && git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_libhardware_legacy refs/changes/29/113229/3 && git cherry-pick FETCH_HEAD
-cd ~/CM13/hardware/libhardware && git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_libhardware refs/changes/54/112454/3 && git cherry-pick FETCH_HEAD
-cd ~
-cd CM13
-
+cd ~/CM13
